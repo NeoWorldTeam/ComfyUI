45f41bba1f664015383bfb98a79b5d7d4c0beb30
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2024-03-22 18:45:41 +0800

    Update instantid_install.sh

--- instantid_install.sh
+++ instantid_install.sh
@@ -7,8 +7,15 @@ cg down nahz202/InstantID_Mo/antelopev2.zip
 
 # 移动文件
 mv InstantID_Mo/ip-adapter.bin models/instantid/
-mv InstantID_Mo/antelopev2.zip models/insightface/models/antelopev2/
+mv InstantID_Mo/antelopev2.zip models/antelopev2/
 mv InstantID_Mo/diffusion_pytorch_model.safetensors models/controlnet/instantid/
 
 # 解压缩antelopev2.zip
-unzip models/insightface/models/antelopev2/antelopev2.zip -d models/insightface/models/antelopev2/
+unzip models/antelopev2/antelopev2.zip -d models/antelopev2/
+
+pip install insightface kornia
+pip uninstall onnxruntime onnxruntime-gpu
+pip install nvidia-pyindex
+pip install onnxruntime
+pip install onnxruntime-gpu --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/
+pip install onnx-graphsurgeon
